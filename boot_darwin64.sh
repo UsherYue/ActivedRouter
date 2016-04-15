3b0834bd9b0d983990d8d01df0649114b06c8126
usher.yue <usher.yue@usheryuedeMacBook-Pro.local>
2016-04-15 12:55:03 +0800

    增加linux 32位 darwin64 安装脚本

---
+++ boot_darwin64.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+# ./xxxx.sh  linux64|darwin64|linux32
+TMP=./tmp/
+GOPATH="/usr/local/GoProject/"
+GOROOT="/usr/local/go/"
+INSTALLPATH="/usr/local/"
+./install.sh darwin64 $TMP $GOPATH $GOROOT $INSTALLPATH
---
+++ boot_linux32.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+# ./xxxx.sh  linux64|darwin64|linux32
+TMP=./tmp/
+GOPATH="/usr/local/GoProject/"
+GOROOT="/usr/local/go/"
+INSTALLPATH="/usr/local/"
+./install.sh linux32 $TMP $GOPATH $GOROOT $INSTALLPATH
