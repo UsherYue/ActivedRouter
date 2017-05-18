56451ad550f0f3d7147c4a8c4b3cb41f4562d72f
usher.yue <usher.yue@gmail.com>
2017-05-18 15:04:23 +0800

    Adjust the configuration file's directory structure

---
+++ boot/config/config_test.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+//load config test
+func TestConfig(t *testing.T) {
+	file, _ := os.Open("../config/client.json")
+	bts, _ := ioutil.ReadAll(file)
+	var server ClientConfigData
+	json.Unmarshal(bts, &server)
+	fmt.Println(server)
+	//	var x map[string]string = map[string]string{"x": "x"}
+	//	fmt.Println((x))
+}
