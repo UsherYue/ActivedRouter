adab1310e8bbf592d62ed905ba1bb3298cb56658
usher.yue <usher.yue@gmail.com>
2017-05-23 18:08:37 +0800

    Add tcp reverse proxy file

---
+++ netservice/tcpproxy.go
@@ -0,0 +1,15 @@
+package netservice
+
+var DefaultTCPProxy = &TCPProxy{}
+
+type TCPProxy struct {
+}
+
+//Load tcp proxy config file..
+func (self *TCPProxy) LoadTCPProxyConfig(configFile string) {
+
+}
+
+func (self *TCPProxy) StartTCPProxy() {
+
+}
